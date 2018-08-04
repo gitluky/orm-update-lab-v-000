499b88973d3091a83c3b863e3db85adefe213e4c
Jonathan Luk <jluk87@gmail.com>
2018-08-04 00:35:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,8 +62,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.create(row[1], row[2])
-    new_student
+    Student.create(row[1], row[2])
+
   end
 
   def self.find_by_name(name)
